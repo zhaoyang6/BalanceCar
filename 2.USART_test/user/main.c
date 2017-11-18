e4d5638875bc970055aed003b11c04e89a3a9859
zhaoyang6 <18701568089@163.com>
2017-11-18 15:22:39 +0800

    增加了串口测试程序

---
+++ 2.USART_test/user/main.c
@@ -0,0 +1,14 @@
+#include "stm32f10x.h"
+#include "usart.h"
+int main()
+{
+//	  USART1_Init();
+//    USART2_Init();
+//  USART3_Init();
+  UART4_Init();
+//  UART5_Init();
+ while(1)
+ {
+   printf("niikmmnvioa\r\n");
+ }
+}
